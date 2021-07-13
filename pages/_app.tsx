7b95da6f63946b0025a499df8c63b228356da5ce
Bruno Ochotorena <hackerhilt@gmail.com>
2021-07-12 23:28:07 -0300

    chore: :art: improve lints

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,26 @@
-import { AppProps } from "next/dist/next-server/lib/router/router";
-import { ReactElement } from "react";
-import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { ReactElement } from 'react'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-    body {
-        background-color: #2da;
-    }
-`;
+  body {
+    background-color: #2da;
+  }
+`
 
 const theme = {
-  colors: {
-    primary: "red",
-  },
-};
+    colors: {
+        primary: 'red',
+    },
+}
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
-  );
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    )
 }
